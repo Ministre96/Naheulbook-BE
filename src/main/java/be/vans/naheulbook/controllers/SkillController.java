b122acf39e203e63755578049d5b864c66f6452f
Baptiste Vansnick <b.vansnick@stag.technofuturtic.education>
2023-03-11 22:03:32 +0100

    feature addskill

--- src/main/java/be/vans/naheulbook/controllers/SkillController.java
+++ src/main/java/be/vans/naheulbook/controllers/SkillController.java
@@ -1,16 +1,20 @@
 package be.vans.naheulbook.controllers;
 
+import be.vans.naheulbook.exceptions.HttpPreConditionFailedException;
+import be.vans.naheulbook.models.dtos.JobDTO;
 import be.vans.naheulbook.models.dtos.OriginDTO;
 import be.vans.naheulbook.models.dtos.SkillDTO;
+import be.vans.naheulbook.models.entities.Job;
+import be.vans.naheulbook.models.entities.Skill;
+import be.vans.naheulbook.models.form.SkillAddForm;
 import be.vans.naheulbook.services.origin.OriginService;
 import be.vans.naheulbook.services.skill.SkillService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @CrossOrigin(origins="http://localhost:4200")
@@ -29,4 +33,16 @@ public class SkillController {
         return ResponseEntity.ok(this.skillService.readAll(pageable).map(SkillDTO:: toDTO).toList());
     }
 
+    @PostMapping("")
+    public ResponseEntity<SkillDTO> addSkillAction(
+            @Valid @RequestBody SkillAddForm skillAddForm
+    ){
+        Skill skill = new Skill();
+        try{
+            skill = this.skillService.save(skillAddForm.toBll());
+        }catch (Exception exception){
+            throw new HttpPreConditionFailedException(exception.getMessage(), new ArrayList<>());
+        }
+        return ResponseEntity.ok(SkillDTO.toDTO(skill));
+    }
 }
---
+++ src/main/java/be/vans/naheulbook/models/form/SkillAddForm.java
@@ -0,0 +1,27 @@
+package be.vans.naheulbook.models.form;
+
+import be.vans.naheulbook.models.entities.Skill;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillAddForm {
+
+    @NotBlank(message = "errors.notBlank")
+    private String name;
+    @NotBlank(message = "errors.notBlank")
+    private String description;
+
+    public Skill toBll(){
+        return Skill.builder()
+                .name(name)
+                .desription(description)
+                .build();
+    }
+}
